a3840b4ab160d8ede9632d503c24a3a1b267b413
Fispe <javifge@gmail.com>
2020-12-11 17:55:38 +0100

    Updates

---
+++ precache-manifest.4562642efe9fbd4fca38b3a5c95a1d36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1174f9e64fc5bda0e86dd84f418131cc",
+    "url": "/music-box-v2/index.html"
+  },
+  {
+    "revision": "c21a2bbca023f1c594e5",
+    "url": "/music-box-v2/static/css/main.fcd684fc.chunk.css"
+  },
+  {
+    "revision": "3e6c8a2e636a791e1c76",
+    "url": "/music-box-v2/static/js/2.eaff8248.chunk.js"
+  },
+  {
+    "revision": "45a97fc5c67750b2a04f1773765102a5",
+    "url": "/music-box-v2/static/js/2.eaff8248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21a2bbca023f1c594e5",
+    "url": "/music-box-v2/static/js/main.6b0b1d50.chunk.js"
+  },
+  {
+    "revision": "9d0fcc5da40e15bcd96a",
+    "url": "/music-box-v2/static/js/runtime-main.b363fb50.js"
+  },
+  {
+    "revision": "a0cb384e5b244fdb2aa923005aaf9908",
+    "url": "/music-box-v2/static/media/default_artwork.a0cb384e.png"
+  },
+  {
+    "revision": "9407832cc361117fb3da5e61f8881ce1",
+    "url": "/music-box-v2/static/media/github2.9407832c.svg"
+  },
+  {
+    "revision": "3ce52b7df6d3e69e5d712619d421fabe",
+    "url": "/music-box-v2/static/media/music_finder.3ce52b7d.svg"
+  },
+  {
+    "revision": "5b003f17faa896395fa2d730981a9df1",
+    "url": "/music-box-v2/static/media/pause.5b003f17.svg"
+  },
+  {
+    "revision": "aef9a9107970f2faa1f723fec9ea466b",
+    "url": "/music-box-v2/static/media/play.aef9a910.svg"
+  },
+  {
+    "revision": "0475bf17b9323c438ef3aa48ac18da42",
+    "url": "/music-box-v2/static/media/rewind.0475bf17.svg"
+  },
+  {
+    "revision": "fa27dd995c95d33960ce5af4b1718dcc",
+    "url": "/music-box-v2/static/media/soundcloud.fa27dd99.svg"
+  },
+  {
+    "revision": "cbec9163ad493c81f5d3faef292e9d97",
+    "url": "/music-box-v2/static/media/sun_moon.cbec9163.svg"
+  }
+]);
